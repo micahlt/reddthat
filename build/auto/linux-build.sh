98992c54db3a1bb9cc85f6e66a2ecb640798284c
Micah Lindley <micahincorporated@gmail.com>
2020-03-03 18:13:10 -0600

    Added an autobuild file for Linux

---
+++ build/auto/linux-build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo -e "Note that this script automatically deletes any files currently in the /dist directory!"
+rm -r ../../dist
+npm i --save-dev electron-builder
+../../node_modules/.bin/electron-builder --linux deb --project ../../
+echo -e "If you didn't get any errors, your executable has been built!"
