4cc7bfddba3fe319ffe810829d86cd40749967d2
Micah Lindley <micahincorporated@gmail.com>
2020-03-28 18:04:36 -0500

    Minor changes

--- build/auto/linux-build.sh
+++ build/auto/linux-build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo -e "Note that this script automatically deletes any files currently in the /dist directory!"
+read -p "Press enter to continue or Ctrl+C to stop execution."
 rm -r ../../dist
 npm i --save-dev electron-builder
 ../../node_modules/.bin/electron-builder --linux deb --project ../../
